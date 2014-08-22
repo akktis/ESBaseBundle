1f8c0beaedc2f9d034b43dd7932f4e83683bfba9
4rthem <arthurdemoulins@gmail.com>
2014-08-22 17:47:47 +0200

    notifications ...

--- Tests/Controller/BaseTest.php
+++ Tests/Controller/BaseTest.php
@@ -3,6 +3,7 @@
 namespace ES\Bundle\BaseBundle\Tests\Controller;
 
 use Doctrine\ORM\EntityManager;
+use ES\Bundle\UserBundle\Model\User;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 abstract class BaseTest extends WebTestCase
@@ -22,5 +23,20 @@ abstract class BaseTest extends WebTestCase
 		}
 
 		return static::$kernel->getContainer();
+	}
+
+	/**
+	 * @param string $userClass
+	 * @param string $username
+	 * @return User
+	 */
+	protected function getUser($userClass, $username)
+	{
+		return $this->getData($userClass, array('username' => $username));
+	}
+
+	protected function getData($dataClass, array $criteria)
+	{
+		return self::getEntityManager()->getRepository($dataClass)->findOneBy($criteria);
 	}
 }
